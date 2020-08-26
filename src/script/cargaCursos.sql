2bede3bf9cb3e43df1730a890ce30737ec6a26f0
Gerson Silva <30gerson30@gmail.com>
2020-08-25 23:21:13 -0300

    Adicionar Readme e script de carga

---
+++ src/script/cargaCursos.sql
@@ -0,0 +1,9 @@
+INSERT INTO curso (id, nome_curso) VALUES (1, 'Sistemas de Informação');
+
+INSERT INTO curso (id, nome_curso) VALUES (2, 'Direito');
+
+INSERT INTO curso (id, nome_curso) VALUES (3, 'Medicina');
+
+INSERT INTO curso (id, nome_curso) VALUES (4, 'Engenharia florestal');
+
+INSERT INTO curso (id, nome_curso) VALUES (5, 'Logística');
